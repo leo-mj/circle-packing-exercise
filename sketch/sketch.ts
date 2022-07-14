0020ab19d5c32782922a6a3a0edc556392ff77f7
Leo <leo.maedje@gmail.com>
2022-07-14 11:35:01 +0100

    got circles moving

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,23 +1,57 @@
+
+let circlesArray: Circle[] = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noLoop();
+  createCirclesArray();
+  loop();
+  
 }
 
+function createCirclesArray() {
+  circlesArray = calculatePackedCircles(windowWidth, windowHeight);
+}
+
+
+
 function draw() {
+  moveAllCircles()
   background('white');
-
-  const circlesArray = calculatePackedCircles(width, height);
-
+  let i = 0;
   for (const c of circlesArray) {
+    fill(palette[i]);
+    if (i < 4) {
+      i++;
+    } else {
+      i = 0;
+    }
     drawCircle(c);
   }
 }
 
+const palette = [
+  "#f8b195",
+  "#f67280",
+  "#c06c84",
+  "#6c5b7b",
+  "#355c7d"
+];
+
 function drawCircle(c: Circle) {
-  const shade = random(50, 100);
-  fill(shade);
+
   noStroke();
   circle(c.pos.x, c.pos.y, c.radius * 2);
+}
+
+// moves each circle
+function moveAllCircles() {
+  circlesArray = circlesArray.map(move);
+}
+
+function move(c: Circle) {
+  c.pos.x += 1;
+  c.pos.y += 2;
+  return c;
 }
 
 // If user clicks, draw() will be called again (eventually)
